fe89e4506ee4e26266bcce2ca69a3ad6e54ea3ea
JavierMoncadaG <e11931239@student.tuwien.ac.at>
2020-12-06 14:27:21 +0100

    asignar valores al tablero

--- src/CatanMain.java
+++ src/CatanMain.java
@@ -5,6 +5,18 @@ public class CatanMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
+	//Turno de jugador
+		int dice1=(int)(Math.random()*6+1);
+		int dice2=(int)(Math.random()*6+1);
+		int sum = dice1 + dice2;
+		
+		System.out.println("El valor de la suma de los dados es " + sum);
+		
+		Hexagono Tablero = new Hexagono();
+		int[] valores = Tablero.setValores();
+		
+		System.out.println("Las posiciones del tablero son " + valores);
 	}
 
 }
---
+++ src/Hexagono.java
@@ -0,0 +1,38 @@
+package src;
+
+import java.util.Random;
+
+public class Hexagono {
+
+	//Atributos de la casilla
+	int Prob;
+	
+	public int [] setValores() {
+		int [] Posicion = new int[Constantes.CasillaRecurso];
+		int [] Puntuacion  = {0,2,12,3,3,11,11,4,4,10,10,5,5,9,9,6,6,8,8};
+		
+		for (int i=0; i<Posicion.length; i++) {
+			
+			int rnd = new Random().nextInt(Puntuacion.length);
+			int val = Puntuacion[rnd];
+			Posicion[i] = val;
+			System.out.println("Posicion " + i + ":" + val);
+			int []copy = new int[Puntuacion.length - 1];
+			
+			for (int i2=0, j=0; i2<Puntuacion.length; i2++) {
+							
+				if (i2 != rnd) {
+					copy[j] = Puntuacion[i2];
+					j++;
+				}
+			}
+			
+			Puntuacion = copy;
+			
+		}
+		return Posicion;
+	}
+	
+	
+}
+
