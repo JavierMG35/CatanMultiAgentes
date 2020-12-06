6d40d4b847593527da77c45456580880b49e49fc
JavierMoncadaG <e11931239@student.tuwien.ac.at>
2020-12-06 12:05:45 +0100

    Constantes class

---
+++ src/Constantes.java
@@ -0,0 +1,33 @@
+package src;
+
+public class Constantes {
+	
+	public static final int Puertos31 = 4;
+	public static final int Puertos21 = 5;
+	
+	public static final int CasillaRecurso = 19;
+	public static final int Bosque = 4;
+	public static final int Prado = 4;
+	public static final int Cultivo = 4;
+	public static final int Colina = 3;
+	public static final int Yacimiento = 3;
+	public static final int Desierto = 1;
+	
+	public static final int CartasRecurso = 95;
+	public static final int Madera = 19;
+	public static final int Lana = 19;
+	public static final int Trigo = 19;
+	public static final int Ladrillo = 19;
+	public static final int Piedra = 19;
+	
+	public static final int CartasDesarrollo = 25;
+	public static final int Caballeros = 14;
+	public static final int PuntosVictoria = 19;
+	
+	public static final int CartasProgreso = 4;
+	public static final int ConstruccionCarretera = 2;
+	public static final int Invencion = 2;
+	public static final int Monopolio = 2;
+	
+	public static final int MaximoPuertos31 = 4;
+}
