1e6bfc41796f5a4086e27a28ac9e9a903b1cbc61
Pablogomez98 <37586098+Pablogomez98@users.noreply.github.com>
2021-01-11 19:18:20 +0100

    clase construccion

---
+++ src/Tablero/Construcción.java
@@ -0,0 +1,23 @@
+package src.Tablero;
+
+public class Construcción {
+	String tipo;
+	public Construcción() {
+		
+	}
+	
+public Construcción(String tipo) {
+		this.tipo = tipo;
+	}
+
+public String getTipo() {
+	return tipo;
+}
+
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+
+
+
+}
